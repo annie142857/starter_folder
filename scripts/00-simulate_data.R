cd7ac178c5857628e89d656f453c0936cceef8b5
annie142857 <156145339+annie142857@users.noreply.github.com>
2024-09-19 18:14:08 -0400

    Update 00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,38 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates Marriage License Data by Date
+# Author: YIng Wen Bu
+# Date: 19 September, 2024
+# Contact: anni.bu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: NOne
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(304)
 
+# Define the start and end date
+start_date <- as.Date("2018-01-01")
+end_date <- as.Date("2023-12-31")
 
+# Set the number of random dates you want to generate
+number_of_dates <- 100
 
+data <- 
+  tibble(
+    dates = as.Date(
+      runif(
+        n = number_of_dates,
+        min = as.numeric(start_date),
+        max = as.numeric(end_date)
+      ),
+      origin = "1970-01-01"
+    ),
+    number_of_marriage = rpois(n = number_of_dates, lambda = 10)
+)
+
+#### Write_csv
+write_csv(data, file = "C:/Users/86189/Downloads/starter_folder-main (1)/starter_folder-main/data/analysis_data/simulated_data.csv")
