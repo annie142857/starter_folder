9ba0556318bc451cb24196cd40e44ff27adf064a
Annie Bu <annie.bu@mail.utoronto.ca>
2024-09-27 13:22:42 -0400

    Simulated and Tested Data Including "FeelingNewCasino", "CasinoFitToronto", "onlyCasino", "noCasino", "casinoAndOthers", "Standalone", "Complex", "Gender".

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,10 +1,10 @@
 #### Preamble ####
-# Purpose: Simulates Marriage License Data by Date
+# Purpose: Simulates Casino Survey Data
 # Author: YIng Wen Bu
 # Date: 19 September, 2024
-# Contact: anni.bu@mail.utoronto.ca
+# Contact: anni2.bu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: NOne
+# Pre-requisites: None
 # Any other information needed? None
 
 
@@ -14,16 +14,43 @@ library(tidyverse)
 #### Simulate data ####
 set.seed(304)
 
-# Define the start and end date
-start_date <- as.Date("2018-01-01")
-end_date <- as.Date("2023-12-31")
-
-# Set the number of random dates you want to generate
-number_of_dates <- 100
+# Set the number of random responses you want to generate
+number_of_responses <- 300
 
 data <- 
   tibble(
-    dates = as.Date(
+    "Number" = 1:number_of_responses,
+    "Feeling new casino" = sample(
+      x = c("Strongly in favour", "Somewhat in favour", "Neutral or mixed feelings", "Somewhat opposed", "Strongly opposed"),
+      size = 300,
+      replace = TRUE
+    ),
+    "Casino fit Toronto" = sample(
+      x = c("Fits Image Perfectly", "Fits Image Somewhat", "Neutral", "Does Not Fit at All"),
+      size = 300,
+      replace = TRUE
+    ),
+    "only casino" = sample(
+      x = c(1, 0),
+      size = 300,
+      replace = TRUE
+    ),
+    "no casino" = sample(
+      x = c(1, 0),
+      size = 300,
+      replace = TRUE
+    ),
+    "casino and others" = sample(
+      x = c("Male", "Female", "Transgender"),
+      size = 300,
+      replace = TRUE
+    ),
+    "Gender" = sample(
+      x = c("Male", "Female", "Transgender"),
+      size = 300,
+      replace = TRUE
+    ),
+              as.Date(
       runif(
         n = number_of_dates,
         min = as.numeric(start_date),
